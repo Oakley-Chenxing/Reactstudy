5ee81be85ce40fee8732c18cfad4033fced046d3
Qakley-Chenxing <Chenxing.li@northumbria.ac.uk>
2021-03-12 01:05:22 +0800

    complete the projrct except mock

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 class TodoItem extends Component{
 constructor(props){
     super(props);
     this.handleClick=this.handleClick.bind(this);
 }
+shouldComponentUpdate(nextProps, nextState){
+    if (nextProps.conent !== this.props.conent){
+        return true;
+    }else{
+        return false;//性能优化,不再多执行子组件render,强制要求不更新
+    }
+    
+}
 
     render(){
-
+        console.log('child render');
         return (
         <div onClick={this.handleClick}>
             {this.props.content}
@@ -19,4 +27,12 @@ constructor(props){
            
 }
 }
+//做校验 接受按要求的值
+TodoItem.propTypes={
+    content: PropTypes.oneOfType([PropTypes.number,PropTypes.string]),//onoftype shuzu
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+}
+//定义默认值 真的没有test值
+
 export default TodoItem;
--- src/Todolist.js
+++ src/Todolist.js
@@ -1,5 +1,6 @@
 import React, {Component,Fragment} from'react';
 import TodoItem from './TodoItem';
+import axios from 'axios';
 import './style.css';
  class Todolist extends Component{
 
@@ -13,7 +14,13 @@ import './style.css';
       this.handleBtnClick=this.handleBtnClick.bind(this);
       this.handleItemDelete=this.handleItemDelete.bind(this);
     }
+    //在组件即将被挂在到页面的时刻执行
+//componentWillUnmount(){
+    //console.log('componentWillMount');
+//}
+
   render(){
+      //console.log('parent render');//看com是否在被挂在之前执行
          return(
              <Fragment>
                  {/*JSX zujian xia mian shi input kuang*/}
@@ -22,24 +29,34 @@ import './style.css';
                      //dan hang zhushi
                  }
                  <label htmlFor="insertArea"> Please Input Content </label>
+                 
                  <input 
                  id= "insertArea"
                  className='input'
                  value={this.state.inputValue}
                 onChange={this.handleInputChange}
+                
                  />
                  <button 
                  onClick={this.handleBtnClick}>submit
                  </button>
                  </div>
-             <ul>
+             <ul >
                 {
                     this.getTodoItem()
                 }
              </ul>
+
              </Fragment>
          )
          }
+
+        componentDidMount(){
+axios.get('/api/todolist')
+.then(() =>{alert('succ')})
+.catch(() =>{alert('error')})
+}
+
          getTodoItem(){
             return this.state.list.map((item,index)=>{
                 return(
@@ -54,25 +71,30 @@ import './style.css';
          }
 
   handleInputChange(e){
-      const value=e.target.value;
+    const value =e.target.value;  
+    //console.log(e.target);
+      //da yin chu e.target zhi  dom jiedian
+      //const value=e.target.value;
     this.setState ( ()=>({
        inputValue: value
     })) ;   
   }
 
   handleBtnClick(){
-    this.setState ( ()=>({
+    this.setState ( (prevState)=>({
         list: [...this.state.list, this.state.inputValue],
         inputValue:''
-     }))   ; 
+     })
+     /*,()=>{
+         console.log(this.ul.querySelectorAll('div').length);
+     }*/
+     ); 
    }
 
 
   handleItemDelete(index){
       //immutable
       //do no correct state
-    
-      
       this.setState ( (prevState)=>{
           const list =[...prevState.list]
           list.splice(index,1);
